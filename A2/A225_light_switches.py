afa19288ad019e07faebed4571b6cba1821d91bb
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 08:13:19 +0000

    A225 input validation refactored for conciseness; comments refined for clarity and to adhere to PEP 8's line length guidelines

--- A2/A225_light_switches.py
+++ A2/A225_light_switches.py
@@ -1,24 +1,26 @@
 num_switches = int(input("Enter number of switches: "))
-#Initialising list containing the current status of each light switch, all starting as being off
+# Initialising list containing the current status of each light switch,
+# where all switches start as being off (False).
 switches = [False] * num_switches
 
 while True:
-    #Takes and splits the input into elements in a list
+    # Takes and splits the input into a list to use as limits.
     limits = input().split(" ")
-    #If an input is non-numerical, or there are not 2 limits, it stops the loop
-    if len(limits) != 2 or not limits[0].isnumeric() or not limits[1].isnumeric():
+    # If a limit is non-numerical, or there are not 2 limits, it stops
+    # the loop.
+    if len(limits) != 2 or not all(i.isnumeric() for i in limits):
         break
     start, end = int(limits[0]), int(limits[1])
-    #Ensures starting limit is less than or equal to the ending limit
+    # Ensures starting limit is less than or equal to the ending limit.
     if start > end:
         start, end = end, start
-    #Ensures limits are within valid range
+    # Ensures limits are within valid range.
     if start < 0 or end >= num_switches:
         print("Error: limits outside of range")
     else:
-        #Reverses the statuses of the switches in the inputted range inclusive
+        # Toggles switches in the specified range (inclusive).
         for i in range(start,end+1):
             switches[i] = not switches[i]
 
-#Counts and ouputs the number of light switches which are currently on
+# Counts and outputs the number of switches that are currently on.
 print(switches.count(True))
