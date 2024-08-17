6d67b0e0f8a7731565317a9f991c6a51917cd2d7
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 09:18:54 +0000

    AS85 updated to adhere to PEP 8's line length guidelines; comments revised to remove redundancies and improve clarity

--- AS/AS85_if_only_i_could_handle_files.py
+++ AS/AS85_if_only_i_could_handle_files.py
@@ -1,39 +1,40 @@
-#Opening the files for reading
+# Opening and reading the text files.
 with open("if.txt") as file1, open("mam.txt") as file2:
-    #Reading the files
     text1 = file1.read()
     text2 = file2.read()
 
-#Initialising variables for counting
+# Counting the number of times 'if' appears in each file.
 count = 0
 if_counter1, if_counter2 = 0, 0
-#Counting the number of times 'if' appears in each file
 while count < len(text1)-1 or count < len(text1)-1:
     if count < len(text1)-1:
         if (text1[count]+text1[count+1]).lower() == "if":
-            #Ensures words which include if (e.g. gift) are not included, but cases where 'if' is next to punctuation are included
-            if ((count == 0 and not text1[count+2].isalpha()) #If count is 0, then cannot check [count-1]
-                or (count == len(text1)-2 and not text1[count-1].isalpha()) #If count == len(text1)-2, then cannot check [count+2]
-                or (not text1[count-1].isalpha() and not text1[count+2].isalpha())): #If isalpha() for either value is true, then the 'if' must be part of a longer word rather than 'if' by itself
+            # Ensures 'if' is not part of a larger word.
+            # If count is 0, [count-1] does not exist
+            if ((count == 0 and not text1[count+2].isalpha())
+                # If count == len(text1)-2, [count+2] does not exist
+                or (count == len(text1)-2 and not text1[count-1].isalpha())
+                or (not text1[count-1].isalpha() and 
+                    not text1[count+2].isalpha())):
                 if_counter1 += 1
     if count < len(text2)-1:
         if (text2[count]+text2[count+1]).lower() == "if":
-            #Ensures words which include if (e.g. gift) are not included, but cases where 'if' is next to punctuation are included
-            if ((count == 0 and not text2[count+2].isalpha()) #If count is 0, then cannot check [count-1]
-                or (count == len(text2)-2 and not text2[count-1].isalpha()) #If count == len(text2)-2, then cannot check [count+2]
-                or (not text2[count-1].isalpha() and not text2[count+2].isalpha())): #If isalpha() for either value is true, then the 'if' must be part of a longer word rather than 'if' by itself
+            # Ensures 'if' is not part of a larger word.
+            # If count is 0, [count-1] does not exist
+            if ((count == 0 and not text2[count+2].isalpha())
+                #If count == len(text2)-2, [count+2] does not exist
+                or (count == len(text2)-2 and not text2[count-1].isalpha()) 
+                or (not text2[count-1].isalpha() and 
+                    not text2[count+2].isalpha())):
                 if_counter2 += 1
     count += 1
 
-#Opening the files for appending
+# Opening the text files for appending
 with open("if.txt","a") as file1, open("mam.txt","a") as file2:
-    #Appending the if counters to their respective files
+    # Appending the if counters to their respective files
     file1.write(str(if_counter1))
     file2.write(str(if_counter2))
-File1 = open("if.txt","a")
-File2 = open("mam.txt","a")
 
-#Stating which file has more of the word 'if'
 if if_counter1 > if_counter2:
     print("File 1 has more of the word 'if' than file 2")
 elif if_counter2 > if_counter1:
