c98538c72fbc75a9a7a8b88667db89cc221af4f2
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 06:22:24 +0000

    A221 comments revised for clarity, compliance with PEP 8's line length guidelines, and removal of redundancies

--- A2/A221_duplicates_removal.py
+++ A2/A221_duplicates_removal.py
@@ -1,14 +1,13 @@
 def duplicates_removal(original):
+    """Return copy of original list with duplicate elements removed."""
     new_list = []
     for element in original:
-        #Checks if current element of the original list is a duplicate
+        # Checks if current element is not a duplicate.
         if element not in new_list:
-            #Only if the current element is not a duplicate will it be added to new_list
             new_list.append(element)
-    #Returns the list without any duplicates
     return new_list
 
-#Takes the users input as a list, with each element in the input seperated by a space
+# Takes the users input as a list, with elements seperated by a space.
 original_list = input("Enter elements of list: ").split(" ")
-#Prints the list without any duplicates
+# Prints the list after removing duplicates.
 print(duplicates_removal(original_list))
