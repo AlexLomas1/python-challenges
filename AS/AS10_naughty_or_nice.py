0e96f59a215c022ade4e93a104127c02623d912c
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 08:44:59 +0000

    AS10 comments revised to remove redundancy, improve clarity and to adhere with PEP 8's line length guidlines

--- AS/AS10_naughty_or_nice.py
+++ AS/AS10_naughty_or_nice.py
@@ -1,40 +1,43 @@
-#Function to determine if a string is considered Naughty or Nice
 def naughty_or_nice(string):
-    #Initialising local variables
+    """
+    Determine if a string is naughty or nice based on specific criteria.
+    Return "Nice" if it contains at least 3 vowels, at least one letter
+    appearing twice in a row, and does not contain any of the substrings
+    "ab", "cd", "pq" or "xy". Otherwise, return "Naughty".
+    """
     count, vowel_count = 0, 0
     double_letter, ab_cd_pq_xy = False, False
-    #Converts string to a list
     string = list(string)
     while count < len(string) and not ab_cd_pq_xy:
-        #Checks if current element in list is a vowel
+        # Counts vowels in the string
         if string[count].lower() in ["a","e","i","o","u"]:
             vowel_count += 1
-        #Necessary because next two if statements both use string[Count+1], and if Count = len(string)-1 then string[Count+1] does not exist
+        # Necessary because next two if statements both use string[
+        # Count+1], and if Count = len(string)-1 then string[Count+1]
+        # does not exist.
         if count < len(string)-1:
-            #Checks if there is a double letter
+            # Checks if there is a double letter.
             if string[count].lower() == string[count+1].lower():
                 double_letter = True
-            #Checks if any of the bad substrings are present
+            # Checks if any of the bad substrings are present.
             if (string[count]+string[count+1]) in ["ab","cd","pq","xy"]:
                 ab_cd_pq_xy = True
         count += 1
-    #Checks from the variable values if the string is naughty or nice
+    # Checks and returns whether the string is naughty or nice.
     if vowel_count >= 3 and double_letter and not ab_cd_pq_xy:
         return "Nice"
     else:
         return "Naughty"
 
-#Opens text file
-#promptspal.txt is used (from AS86) simply because I don't have another text file to test code with
+# Opens text file, promptspal.txt is used simply for testing purposes.
 with open("promptspal.txt") as text_file:
-    #Reads the text file and saves it in a list, with each line as an element
-    #Could  instead use text = (text_file.read()).split(" ") to seperate file into words rather than lines
+    # Reads and saves text file as a list, with each line as an element.
+    # Could instead use text = (text_file.read()).split(" ") to seperate
+    # file into words rather than lines.
     text = text_file.readlines()
 
-#Initialises counters for the number of nice and naughty strings
-nice_count = 0
-naughty_count = 0
-#Checks if current string is considered naughty or nice, and updates counter variables accordingly
+# Counts number of naughty strings and nice strings
+nice_count, naughty_count = 0, 0
 for line in text:
     if naughty_or_nice(line) == "Nice":
         nice_count += 1
