67a27fc24465335edd3815e61a404ef29c17c500
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 05:52:23 +0000

    A218 comments refined for clarity and to adhere to PEP 8's line length guidelines

--- A2/A218_triangular_numbers.py
+++ A2/A218_triangular_numbers.py
@@ -1,27 +1,25 @@
-#Array to store triangle numbers
 triangle_nums = [1]
-#Function to calculate and store the next triangle number in the list
+# Function to calculate and store the next triangle number in the list
 def next_triangle_num(n):
     triangle_nums.append(0.5*n*(n+1))
 
-#Opens the text file
+# Opens and reads the text file
 with open("words.txt") as words:
-    #Reads the text file
     words_list = words.read()
 
-#Variable used to count the number of triangle words
 triangle_word_count = 0
 
-#Calculates the value of each word in words_list
+# Calculates the value of each word in words_list
 for word in words_list:
     word_value = 0
     for letter in word:
-        #ord(letter.upper())-64 returns the alphabetical position of letter (e.g. if letter is "a" it would return 1)
+        # ord(letter.upper())-64 returns the alphabetical position of 
+        # letter (e.g. if letter is "a" it would return 1).
         word_value += ord(letter.upper())-64
-    #Causes the next triangle number to be calculated if the current value exceeds the highest stored triangle number
+    # Causes the next triangle number to be calculated if the current
+    # value exceeds the highest stored triangle number.
     while word_value > triangle_nums[len(triangle_nums)-1]:
         next_triangle_num(len(triangle_nums)+1)
-    #Counts the number of word value's which are triangle numbers
     if word_value in triangle_nums:
         triangle_word_count += 1
 
