b98929de63bf0ecc7f9b5c48704ecb0639ed83a6
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-17 08:57:00 +0000

    AS74 edited to better conform to Python naming conventions

--- AS/AS74_logic_gates.py
+++ AS/AS74_logic_gates.py
@@ -1,17 +1,17 @@
-def NAND(A,B): #Creating NAND Gate to be used in the other logic gates
-    return not (A and B)
+def nand(a,b): # Creating NAND Gate to be used in the other logic gates
+    return not (a and b)
 
-def AND_Gate(A,B):
-    return NAND(NAND(A,B),NAND(A,B))
+def and_gate(a,b):
+    return nand(nand(a,b),nand(a,b))
 
-def OR_Gate(A,B):
-    return NAND(NAND(A,A),NAND(B,B))
+def or_gate(a,b):
+    return nand(nand(a,a),nand(b,b))
 
-def NOR_Gate(A,B):
-    return NAND(NAND(NAND(A,A),NAND(B,B)),NAND(NAND(A,A),NAND(B,B)))
+def nor_gate(a,b):
+    return nand(nand(nand(a,a),nand(b,b)),nand(nand(a,a),nand(b,b)))
 
-def XOR_Gate(A,B):
-    return NAND(NAND(NAND(A,A),B),NAND(NAND(B,B),A))
+def xor_gate(a,b):
+    return nand(nand(nand(a,a),b),nand(nand(b,b),a))
 
-def NOT_Gate(A):
-    return(NAND(A,A))
+def not_gate(a):
+    return(nand(a,a))
