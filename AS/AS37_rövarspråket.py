0061956ed1d1cb788a2ada58b0909571263c1dd1
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-08-31 13:35:34 +0000

    AS37 edited to correct logic error

--- AS/AS37_rövarspråket.py
+++ AS/AS37_rövarspråket.py
@@ -1,23 +1,27 @@
-# Takes user input for original string and converts to a list
+# Takes user input for original string and converts to a list.
 original = list(input("Enter text to convert to Rövarspråket: "))
 consonants = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t",
               "v","w","x","y""z"}
 converted = []
 consonant_sequence = ""
 
-for element in original:
-    if element.lower() not in consonants:
-        # If previous element was a consonant, it is now added to converted 
+for i in range(len(original)):
+    if original[i].lower() not in consonants:
+        # If previous element was a consonant, it is now added to converted .
         if consonant_sequence:
-            # Adds the consonant(s) to converted in Rövarspråket format
+            # Adds the consonant(s) to converted in Rövarspråket format.
             converted.append(consonant_sequence+"o"+consonant_sequence.lower())
             consonant_sequence = ""
-        # Adds current (non-consonant) element to converted
-        converted.append(element)
+        # Adds current (non-consonant) element to converted.
+        converted.append(original[i])
     else:
         # Stores consonant elements in consonant_sequence, to later 
         # convert them to Rövarspråket format. Used to deal with
         # consecutive consonants.
-        consonant_sequence += element
+        consonant_sequence += original[i]
+        # Ensures consonant(s) are added to converted even if final 
+        # element of original is a consonant.
+        if i == len(original)-1:
+            converted.append((consonant_sequence+"o"+consonant_sequence.lower()))
         
 print("".join(converted))
