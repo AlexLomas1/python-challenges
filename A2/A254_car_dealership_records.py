5bcb9369973ec9b1313ba21d7828c7069732095a
AlexLomas1 <129366872+AlexLomas1@users.noreply.github.com>
2024-09-05 07:31:21 +0000

    A254 edited to avoid using globals() by storing objects in a dictionary, to add a loop exit condition and to improve readability

--- A2/A254_car_dealership_records.py
+++ A2/A254_car_dealership_records.py
@@ -1,5 +1,6 @@
 class CarRecord:
-    def __init__(self, _vehicle_id, _registration, _date_of_registration, _engine_size, _purchase_price):
+    def __init__(self, _vehicle_id, _registration, _date_of_registration,
+                 _engine_size, _purchase_price):
         """Initialise the CarRecord object."""
         self.vehicle_id = _vehicle_id
         self.registration = _registration
@@ -7,17 +8,19 @@ class CarRecord:
         self.engine_size = _engine_size
         self.purchase_price = _purchase_price
 
-# Creates empty list to store the objects of CarRecord.
-records = []
+# Creates empty dictionary to store the objects of CarRecord.
+records = {}
 # Repeats to allow for any amount of objects to be created.
 while True:
     record_name = input("Enter record name: ")
+    if record_name.lower() == "exit":
+        break
     vehicle_id = input("Enter vehicle ID: ")
     registration = input("Enter registration: ")
     date_of_registration = input("Enter date of registration: ")
     engine_size = input("Enter engine size: ")
     purchase_price = input("Enter purchase price: ")
-    # Creates CarRecord object with the name record_name
-    globals()[record_name] = CarRecord(vehicle_id, registration, date_of_registration, engine_size, purchase_price)
-    # Adds current CarRecord object to the list records.
-    records.append(globals()[record_name])
+    # Creates CarRecord object and stores in records, with record_name
+    # as the key.
+    records[record_name] = CarRecord(vehicle_id, registration, date_of_registration,
+                                     engine_size, purchase_price)
